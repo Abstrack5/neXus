56b3ce0d5cf9b76a058b5fb993932ff965e64468
Abstrack5 <armon_ah05@outlook.com>
2022-07-08 00:47:41 -0400

    working on user models

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = newSchema({
+   username: {
+    type: String,
+    unique: true,
+    required: 'Please provide a username',
+    trim: true
+   },
+   email: {
+    type: String,
+    unique: true,
+    required: 'Please provide an email address',
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+   },
+   thoughts: {
+
+   },
+   friends: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+   },  
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+)
+
+UserSchema.virtual('friendCount').get(function(){
+    return this.friends.length
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
